e7e2ffa14e88c8363245499df5d590d23344c8a6
kentmuzikweb-sudo <kentmuzikweb@gmail.com>
2025-12-11 16:21:03 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
   base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
+    emptyOutDir: true,
     rollupOptions: {
+      input: {
+        main: './index.html',
+      },
       output: {
         manualChunks: undefined,
       },
